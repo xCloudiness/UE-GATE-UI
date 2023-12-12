4b26d87c69c4f406311c36a7be1c6fd9e4eb832f
jessi <jessi@SAM18>
2023-12-13 02:25:08 +0800

    Add project files.

---
+++ Dashboard/GateUI.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Dashboard
+{
+    partial class GateUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GateUI));
+            this.ConnectDB = new System.Windows.Forms.Button();
+            this.InputBox = new System.Windows.Forms.GroupBox();
+            this.OKInput2 = new System.Windows.Forms.Button();
+            this.OKInput1 = new System.Windows.Forms.Button();
+            this.IDInput2 = new System.Windows.Forms.TextBox();
+            this.IDInput1 = new System.Windows.Forms.TextBox();
+            this.CurrentDate = new System.Windows.Forms.Label();
+            this.CurrentTime = new System.Windows.Forms.Label();
+            this.ClockTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ViolationBox2 = new System.Windows.Forms.ComboBox();
+            this.StudentID2 = new System.Windows.Forms.Label();
+            this.StudentName2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Violations = new System.Windows.Forms.ComboBox();
+            this.StudentID1 = new System.Windows.Forms.Label();
+            this.StudentName1 = new System.Windows.Forms.Label();
+            this.StudentImage2 = new System.Windows.Forms.PictureBox();
+            this.StudentImage1 = new System.Windows.Forms.PictureBox();
+            this.InputBox.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentImage2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentImage1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ConnectDB
+            // 
+            this.ConnectDB.Location = new System.Drawing.Point(0, 0);
+            this.ConnectDB.Name = "ConnectDB";
+            this.ConnectDB.Size = new System.Drawing.Size(110, 31);
+            this.ConnectDB.TabIndex = 2;
+            this.ConnectDB.Text = "DB TEST";
+            this.ConnectDB.UseVisualStyleBackColor = true;
+            this.ConnectDB.Click += new System.EventHandler(this.ConnectDB_Click);
+            // 
+            // InputBox
+            // 
+            this.InputBox.Controls.Add(this.OKInput2);
+            this.InputBox.Controls.Add(this.OKInput1);
+            this.InputBox.Controls.Add(this.IDInput2);
+            this.InputBox.Controls.Add(this.IDInput1);
+            this.InputBox.Controls.Add(this.CurrentDate);
+            this.InputBox.Controls.Add(this.CurrentTime);
+            this.InputBox.Controls.Add(this.ConnectDB);
+            this.InputBox.Location = new System.Drawing.Point(12, 25);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(358, 722);
+            this.InputBox.TabIndex = 3;
+            this.InputBox.TabStop = false;
+            // 
+            // OKInput2
+            // 
+            this.OKInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.OKInput2.Location = new System.Drawing.Point(232, 364);
+            this.OKInput2.Name = "OKInput2";
+            this.OKInput2.Size = new System.Drawing.Size(68, 23);
+            this.OKInput2.TabIndex = 8;
+            this.OKInput2.Text = "OK";
+            this.OKInput2.UseVisualStyleBackColor = true;
+            this.OKInput2.Click += new System.EventHandler(this.OKInput2_Click);
+            // 
+            // OKInput1
+            // 
+            this.OKInput1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.OKInput1.Location = new System.Drawing.Point(236, 176);
+            this.OKInput1.Name = "OKInput1";
+            this.OKInput1.Size = new System.Drawing.Size(68, 23);
+            this.OKInput1.TabIndex = 7;
+            this.OKInput1.Text = "OK";
+            this.OKInput1.UseVisualStyleBackColor = true;
+            this.OKInput1.Click += new System.EventHandler(this.OKInput1_Click);
+            // 
+            // IDInput2
+            // 
+            this.IDInput2.Location = new System.Drawing.Point(70, 361);
+            this.IDInput2.Name = "IDInput2";
+            this.IDInput2.Size = new System.Drawing.Size(153, 26);
+            this.IDInput2.TabIndex = 6;
+            this.IDInput2.TextChanged += new System.EventHandler(this.IDInput2_TextChanged);
+            // 
+            // IDInput1
+            // 
+            this.IDInput1.Location = new System.Drawing.Point(70, 176);
+            this.IDInput1.Name = "IDInput1";
+            this.IDInput1.Size = new System.Drawing.Size(153, 26);
+            this.IDInput1.TabIndex = 5;
+            this.IDInput1.TextChanged += new System.EventHandler(this.IDInput1_TextChanged);
+            // 
+            // CurrentDate
+            // 
+            this.CurrentDate.AutoSize = true;
+            this.CurrentDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.CurrentDate.Location = new System.Drawing.Point(79, 598);
+            this.CurrentDate.Name = "CurrentDate";
+            this.CurrentDate.Size = new System.Drawing.Size(186, 36);
+            this.CurrentDate.TabIndex = 4;
+            this.CurrentDate.Text = "CurrentDate";
+            this.CurrentDate.Click += new System.EventHandler(this.CurrentDate_Click);
+            // 
+            // CurrentTime
+            // 
+            this.CurrentTime.AutoSize = true;
+            this.CurrentTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.CurrentTime.Location = new System.Drawing.Point(79, 541);
+            this.CurrentTime.Name = "CurrentTime";
+            this.CurrentTime.Size = new System.Drawing.Size(189, 36);
+            this.CurrentTime.TabIndex = 3;
+            this.CurrentTime.Text = "CurrentTime";
+            this.CurrentTime.Click += new System.EventHandler(this.CurrentTime_Click);
+            // 
+            // ClockTimer
+            // 
+            this.ClockTimer.Enabled = true;
+            this.ClockTimer.Interval = 1000;
+            this.ClockTimer.Tick += new System.EventHandler(this.ClockTimer_Tick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.ViolationBox2);
+            this.groupBox2.Controls.Add(this.StudentID2);
+            this.groupBox2.Controls.Add(this.StudentName2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.Violations);
+            this.groupBox2.Controls.Add(this.StudentID1);
+            this.groupBox2.Controls.Add(this.StudentName1);
+            this.groupBox2.Controls.Add(this.StudentImage2);
+            this.groupBox2.Controls.Add(this.StudentImage1);
+            this.groupBox2.Location = new System.Drawing.Point(432, 25);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(798, 667);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 436);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Violations:";
+            // 
+            // ViolationBox2
+            // 
+            this.ViolationBox2.FormattingEnabled = true;
+            this.ViolationBox2.Items.AddRange(new object[] {
+            "(None)",
+            "Uniform Violation",
+            "ID Violation"});
+            this.ViolationBox2.Location = new System.Drawing.Point(328, 478);
+            this.ViolationBox2.Name = "ViolationBox2";
+            this.ViolationBox2.Size = new System.Drawing.Size(176, 28);
+            this.ViolationBox2.TabIndex = 20;
+            this.ViolationBox2.SelectedIndexChanged += new System.EventHandler(this.ViolationBox2_SelectedIndexChanged);
+            // 
+            // StudentID2
+            // 
+            this.StudentID2.AutoSize = true;
+            this.StudentID2.Location = new System.Drawing.Point(324, 395);
+            this.StudentID2.Name = "StudentID2";
+            this.StudentID2.Size = new System.Drawing.Size(110, 20);
+            this.StudentID2.TabIndex = 19;
+            this.StudentID2.Text = "Student ID: ";
+            // 
+            // StudentName2
+            // 
+            this.StudentName2.AutoSize = true;
+            this.StudentName2.Location = new System.Drawing.Point(324, 358);
+            this.StudentName2.Name = "StudentName2";
+            this.StudentName2.Size = new System.Drawing.Size(69, 20);
+            this.StudentName2.TabIndex = 18;
+            this.StudentName2.Text = "Name: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(324, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Violations:";
+            // 
+            // Violations
+            // 
+            this.Violations.FormattingEnabled = true;
+            this.Violations.Items.AddRange(new object[] {
+            "(None)",
+            "Uniform Violation",
+            "ID Violation"});
+            this.Violations.Location = new System.Drawing.Point(328, 216);
+            this.Violations.Name = "Violations";
+            this.Violations.Size = new System.Drawing.Size(176, 28);
+            this.Violations.TabIndex = 16;
+            this.Violations.SelectedIndexChanged += new System.EventHandler(this.Violations_SelectedIndexChanged);
+            // 
+            // StudentID1
+            // 
+            this.StudentID1.AutoSize = true;
+            this.StudentID1.Location = new System.Drawing.Point(324, 133);
+            this.StudentID1.Name = "StudentID1";
+            this.StudentID1.Size = new System.Drawing.Size(110, 20);
+            this.StudentID1.TabIndex = 15;
+            this.StudentID1.Text = "Student ID: ";
+            // 
+            // StudentName1
+            // 
+            this.StudentName1.AutoSize = true;
+            this.StudentName1.Location = new System.Drawing.Point(324, 96);
+            this.StudentName1.Name = "StudentName1";
+            this.StudentName1.Size = new System.Drawing.Size(69, 20);
+            this.StudentName1.TabIndex = 14;
+            this.StudentName1.Text = "Name: ";
+            // 
+            // StudentImage2
+            // 
+            this.StudentImage2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.StudentImage2.Location = new System.Drawing.Point(107, 354);
+            this.StudentImage2.Name = "StudentImage2";
+            this.StudentImage2.Size = new System.Drawing.Size(195, 187);
+            this.StudentImage2.TabIndex = 11;
+            this.StudentImage2.TabStop = false;
+            // 
+            // StudentImage1
+            // 
+            this.StudentImage1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.StudentImage1.Location = new System.Drawing.Point(107, 88);
+            this.StudentImage1.Name = "StudentImage1";
+            this.StudentImage1.Size = new System.Drawing.Size(195, 187);
+            this.StudentImage1.TabIndex = 10;
+            this.StudentImage1.TabStop = false;
+            // 
+            // GateUI
+            // 
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1313, 769);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.InputBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "GateUI";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.InputBox.ResumeLayout(false);
+            this.InputBox.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentImage2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentImage1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ConnectDB;
+        private System.Windows.Forms.GroupBox InputBox;
+        private System.Windows.Forms.Label CurrentTime;
+        private System.Windows.Forms.Timer ClockTimer;
+        private System.Windows.Forms.Label CurrentDate;
+        private System.Windows.Forms.TextBox IDInput1;
+        private System.Windows.Forms.TextBox IDInput2;
+        private System.Windows.Forms.Button OKInput2;
+        private System.Windows.Forms.Button OKInput1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox StudentImage2;
+        private System.Windows.Forms.PictureBox StudentImage1;
+        private System.Windows.Forms.Label StudentID1;
+        private System.Windows.Forms.Label StudentName1;
+        private System.Windows.Forms.ComboBox Violations;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ViolationBox2;
+        private System.Windows.Forms.Label StudentID2;
+        private System.Windows.Forms.Label StudentName2;
+    }
+}
+
---
+++ Dashboard/GateUI.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Dashboard
+{
+    public partial class GateUI : Form
+    {
+        string dbcon = "localhost";
+        string mysqluser = "root";
+        string mysqlpassword = "";
+        string dbname = "studentdb";
+
+        public GateUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CurrentTime.Text = DateTime.Now.ToLongTimeString();
+            CurrentDate.Text = DateTime.Now.ToShortDateString();
+            this.IDInput1.Enter += new EventHandler(IDInput1_Enter);
+            this.IDInput1.Leave += new EventHandler(IDInput1_Leave);
+            this.IDInput2.Enter += new EventHandler(IDInput2_Enter);
+            this.IDInput2.Leave += new EventHandler(IDInput2_Leave);
+            IDInput1_SetText();
+            IDInput2_SetText();
+
+        }
+
+        protected void IDInput1_SetText()
+        {
+            this.IDInput1.Text = "Enter Student ID";
+            IDInput1.ForeColor = Color.Gray;
+        }
+
+        private void IDInput1_Enter(object sender, EventArgs e)
+        {
+            if (IDInput1.ForeColor == Color.Black)
+                return;
+            IDInput1.Text = "";
+            IDInput1.ForeColor = Color.Black;
+        }
+        private void IDInput1_Leave(object sender, EventArgs e)
+        {
+            if (IDInput1.Text.Trim() == "")
+                IDInput1_SetText();
+        }
+        protected void IDInput2_SetText()
+        {
+            this.IDInput2.Text = "Enter Student ID";
+            IDInput2.ForeColor = Color.Gray;
+        }
+
+        private void IDInput2_Enter(object sender, EventArgs e)
+        {
+            if (IDInput2.ForeColor == Color.Black)
+                return;
+            IDInput2.Text = "";
+            IDInput2.ForeColor = Color.Black;
+        }
+        private void IDInput2_Leave(object sender, EventArgs e)
+        {
+            if (IDInput2.Text.Trim() == "")
+                IDInput2_SetText();
+        }
+
+        private void ConnectDB_Click(object sender, EventArgs e)
+        {
+            string conString = "server=" + dbcon + ";uid=" + mysqluser + ";pwd=" + mysqlpassword + ";database=" + dbname;
+            MySqlConnection con = new MySqlConnection(conString);
+            try
+            {
+                con.Open();
+                MessageBox.Show("Connection Open!");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open connection!");
+            }
+        }
+
+        private void CurrentTime_Click(object sender, EventArgs e)
+        {
+            ClockTimer.Start();
+        }
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            CurrentTime.Text = DateTime.Now.ToLongTimeString();
+            CurrentDate.Text = DateTime.Now.ToShortDateString();
+            ClockTimer.Start();
+        }
+
+        private void CurrentDate_Click(object sender, EventArgs e)
+        {
+            ClockTimer.Start();
+        }
+
+        private void IDInput1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IDInput2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OKInput1_Click(object sender, EventArgs e)
+        {
+            string conString = "server=" + dbcon + ";uid=" + mysqluser + ";pwd=" + mysqlpassword + ";database=" + dbname;
+            MySqlConnection con = new MySqlConnection(conString);
+            con.Open();
+            try
+            {
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "SELECT FirstName,LastName,SID,studentimage FROM students WHERE SID = " + IDInput1.Text;
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    MessageBox.Show("No records found.");
+                }
+                while (reader.Read())
+                {
+                    StudentName1.Text = "Name: " + reader.GetString(0) + " " + reader.GetString(1);
+                    StudentID1.Text = "Student ID: " + reader.GetString(2);
+                    byte[] imageBytes = Convert.FromBase64String(reader.GetString(3));
+                    using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+                    {
+                        StudentImage1.Image = Image.FromStream(ms, true);
+                        StudentImage1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            CreateTimeIn(IDInput1.Text);
+        }
+
+        private void CreateTimeIn(string studentID)
+        {
+            string conString = "server=" + dbcon + ";uid=" + mysqluser + ";pwd=" + mysqlpassword + ";database=" + dbname;
+            MySqlConnection con = new MySqlConnection(conString);
+
+            try
+            {
+                con.Open();
+
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "INSERT INTO attendance (sid) VALUES(@studentId)";
+                cmd.Parameters.AddWithValue("@studentId", studentID);
+                cmd.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error!");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void OKInput2_Click(object sender, EventArgs e)
+        {
+
+            {
+                string conString = "server=" + dbcon + ";uid=" + mysqluser + ";pwd=" + mysqlpassword + ";database=" + dbname;
+                MySqlConnection con = new MySqlConnection(conString);
+                con.Open();
+                try
+                {
+                    MySqlCommand cmd = con.CreateCommand();
+                    cmd.CommandText = "SELECT FirstName,LastName,SID,studentimage FROM students WHERE SID = " + IDInput2.Text;
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    if (!reader.HasRows)
+                    {
+                        MessageBox.Show("No records found.");
+                    }
+                    while (reader.Read())
+                    {
+                        StudentName2.Text = "Name: " + reader.GetString(0) + " " + reader.GetString(1);
+                        StudentID2.Text = "Student ID: " + reader.GetString(2);
+                        byte[] imageBytes = Convert.FromBase64String(reader.GetString(3));
+                        using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+                        {
+                            StudentImage2.Image = Image.FromStream(ms, true);
+                            StudentImage2.SizeMode = PictureBoxSizeMode.StretchImage;
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+                CreateTimeIn(IDInput2.Text);
+
+            }
+        }
+
+        private void Violations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CreateViolation(Violations.Text, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), IDInput1.Text);
+        }
+        private void CreateViolation(string violationName, string dateTime, string studentID)
+        {
+            string conString = "server=" + dbcon + ";uid=" + mysqluser + ";pwd=" + mysqlpassword + ";database=" + dbname;
+            MySqlConnection con = new MySqlConnection(conString);
+
+            try
+            {
+                if (violationName == "(None)") return;
+
+                con.Open();
+
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "INSERT INTO violations (violation_name, timestamp, sid) VALUES( @violationName, @timeStamp, @studentId)";
+                cmd.Parameters.AddWithValue("@violationName", violationName);
+                cmd.Parameters.AddWithValue("@timeStamp", dateTime);
+                cmd.Parameters.AddWithValue("@studentId", studentID);
+                cmd.ExecuteReader();
+                MessageBox.Show("Violation Added!");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Invalid Input!");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void ViolationBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            CreateViolation(Violations.Text, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), IDInput2.Text);
+        }
+    }
+}
